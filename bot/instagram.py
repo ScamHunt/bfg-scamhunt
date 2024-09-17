1bdd0d8ef351360f1d599856f51b607f12a44041
Kazymir Rabier <kazymir.rabier@gmail.com>
2024-09-17 23:44:19 +0800

    Improve instagram post fetching (#5)
    * refactor + fix case when insta post is private

--- bot/instagram.py
+++ bot/instagram.py
@@ -1,13 +1,36 @@
 from aiograpi import Client
-from aiograpi.types import Media
+from aiograpi.types import Media, UserShort
+from aiograpi.exceptions import PleaseWaitFewMinutes, LoginRequired
+from telegram import Update
+from telegram.constants import ParseMode
+from urllib.parse import urlparse
+import logging
+import re
 
 
 cl = Client()
 
 
-async def extract_post_info(code) -> Media:
-        media_pk = await cl.media_pk_from_code(code)
-        media_info = await cl.media_info(media_pk)
-        return media_info
-
-
+async def handle_instagram(update: Update, link: str):
+    await update.message.reply_text(
+        f"I've detected an Instagram link in your message: {link}\n"
+        "Let me analyse it further..."
+    )
+    try:
+        media_pk = await cl.media_pk_from_url(link)
+        logging.debug(f"Extracted media_pk: {media_pk}")
+        if not media_pk:
+            await update.message.reply_text("I could not extract the post ID from this link, please try again with a valid Instagram post link.")
+            return
+        post = await cl.media_info(media_pk)
+        await update.message.reply_text("I've extracted the following information from this post:\n"
+                                        f"<b>Username</b>: {post.user.username}\n"
+                                        f"<b>Type</b>: {post.media_type}\n"
+                                        f"<b>Caption</b>: <pre>{post.caption_text}</pre>\n",
+                                        parse_mode=ParseMode.HTML) # Careful with Markdown, causes errors because of unescaped chars
+    except LoginRequired as e:
+        logging.error(e)
+        await update.message.reply_text("This Instagram post is private, try submitting a screenshot instead.")
+    except Exception as e:
+        logging.error(e)
+        await update.message.reply_text("I could not analyse this link right now, you can try again later.")
