7a09e9644d9e3817bb02ab40804c2bf277b2caee
Kazymir Rabier <kazymir.rabier@gmail.com>
2024-09-20 19:32:32 +0800

    initial conversation text

---
+++ bot/onboarding/onboarding.py
@@ -0,0 +1,16 @@
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import ContextTypes, ConversationHandler
+
+
+# GENDER, PHOTO, LOCATION, BIO = range(4)
+
+
+# onboarding = ConversationHandler(
+#     entry_points=[CommandHandler("onboarding", start)], states={}
+# )
